50824710454811b73f120b1908fa152753e9e166
joy <hassjoy26@gmail.com>
2025-08-19 10:42:05 +0300

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
-#!/usr/bin/env python3
+import sys
 
-class Book:
-    pass
+class Book(object):
+    def __init__(self, title, page_count):
+        self.title = title
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            sys.stdout.write(u"page_count must be an integer\n")
     
-        
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            sys.stdout.write(u"page_count must be an integer\n")
+
+    def turn_page(self):
+        sys.stdout.write(u"Flipping the page...wow, you read fast!\n")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
-#!/usr/bin/env python3
+import sys
 
-class Shoe:
-    pass
+class Shoe(object):
+    def __init__(self, brand, size):
+        self.brand = brand
+        if isinstance(size, int):
+            self._size = size
+        else:
+            sys.stdout.write(u"size must be an integer\n")
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            sys.stdout.write(u"size must be an integer\n")
+    
+    def cobble(self):
+        sys.stdout.write(u"Your shoe is as good as new!\n")
+        self.condition = "New"
